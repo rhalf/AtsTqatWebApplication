d865e789dd29fcb93fe2e225b31ffe411cc89e15
Rhalf Wendel David Caacbay <rhalfcaacbay@gmail.com>
2015-03-27 12:03:02 +0300

    Updates 20150327
    -Reload Latest

--- connect/connection.php
+++ connect/connection.php
@@ -1,7 +1,12 @@
 <?php
-
 include ("conf.php");
-$Conn = new PDO(
-        "$engine:host=$dbhost;dbname=$prefix$dbname", "$dbuser", "$dbpass", array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
-);
+
+try {
+	$Conn = new PDO(
+	        "$engine:host=$dbhost;dbname=$prefix$dbname", "$dbuser", "$dbpass", array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
+	); 
+} catch (PDOExcemption $cExcemption) {
+	echo ($cExcemption);
+}
+
 ?>
